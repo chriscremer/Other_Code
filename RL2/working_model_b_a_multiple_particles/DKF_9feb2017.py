95b753cf18614e6285ddacd456b4829f963d3f00
Chris <chris.a.cremer@gmail.com>
2017-05-02 16:53:41 -0400

    cleaned code a little

--- RL2/working_model_b_a_multiple_particles/DKF_9feb2017.py
+++ RL2/working_model_b_a_multiple_particles/DKF_9feb2017.py
@@ -25,13 +25,11 @@ class DKF():
         tf.reset_default_graph()
 
         self.batch_size = batch_size
+        self.n_particles = n_particles
 
         self.transfer_fct=tf.nn.softplus #tf.tanh
         self.learning_rate = 0.001
-
-        # self.n_time_steps = n_time_steps #this shouldnt be used
-        self.n_particles = n_particles
-
+        
         self.z_size = network_architecture["n_z"]
         self.input_size = network_architecture["n_input"]
         self.action_size = network_architecture["n_actions"]
@@ -53,16 +51,6 @@ class DKF():
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, epsilon=1e-02).minimize(self.cost)
 
         #Evaluation
-        # self.generate = self.generate(self.x, self.actions)
-        # self.current_z = tf.placeholder(tf.float32, [None, self.z_size])
-        # self.prev_x = tf.placeholder(tf.float32, [None, self.input_size])
-        # self.current_emission = self.observation_net(tf.concat(1, [self.current_z, self.prev_x]), self.network_weights)
-
-        # self.prev_z = tf.placeholder(tf.float32, [None, self.z_size])
-        # self.current_action = tf.placeholder(tf.float32, [None, self.action_size])
-        # apz = tf.concat(1, [tf.concat(1, [self.current_action, self.prev_x]), self.prev_z])
-
-
         self.prev_z_and_current_a_ = tf.placeholder(tf.float32, [self.batch_size, self.z_size+self.action_size])
         self.next_state = self.transition_net(self.prev_z_and_current_a_)
 
@@ -122,10 +110,6 @@ class DKF():
 
         all_weights['decoder_weights']['out_mean'] = tf.Variable(xavier_init(network_architecture['decoder_net'][-1], self.input_size))
         all_weights['decoder_biases']['out_mean'] = tf.Variable(tf.zeros([self.input_size], dtype=tf.float32))
-        # all_weights['decoder_weights']['out_log_var'] = tf.Variable(xavier_init(network_architecture['decoder_net'][-1], self.input_size))
-        # all_weights['decoder_biases']['out_log_var'] = tf.Variable(tf.zeros([self.input_size], dtype=tf.float32))
-
-
 
         #Generator/Transition net q(z|z-1,u)
         all_weights['trans_weights'] = {}
@@ -169,46 +153,20 @@ class DKF():
         output is [B]
         '''
 
-        # term1 = tf.log(tf.reduce_prod(tf.exp(log_var_sq), reduction_indices=1))
-        term1 = tf.reduce_sum(log_var, reduction_indices=1) #sum over dimensions n_z so now its [B]
+        term1 = tf.reduce_sum(log_var, reduction_indices=1) #sum over dimensions, [B]
 
         # [1]
         term2 = self.z_size * tf.log(2*math.pi)
 
         dif = tf.square(z - mean)
         dif_cov = dif / tf.exp(log_var)
-        term3 = tf.reduce_sum(dif_cov, 1) #sum over dimensions so its [B]
+        term3 = tf.reduce_sum(dif_cov, 1) #sum over dimensions,[B]
 
         all_ = term1 + term2 + term3
         log_norm = -.5 * all_
 
         return log_norm
 
-
-    # def log_normal_x(self, x, mean, log_var):
-    #     '''
-    #     Log of normal distribution
-
-    #     z is [B, Z]
-    #     mean is [B, Z]
-    #     log_var is [B, Z]
-    #     output is [B]
-    #     '''
-
-    #     # term1 = tf.log(tf.reduce_prod(tf.exp(log_var_sq), reduction_indices=1))
-    #     term1 = tf.reduce_sum(log_var, reduction_indices=1) #sum over dimensions n_z so now its [B]
-
-    #     # [1]
-    #     term2 = self.input_size * tf.log(2*math.pi)
-
-    #     dif = tf.square(x - mean)
-    #     dif_cov = dif / tf.exp(log_var)
-    #     term3 = tf.reduce_sum(dif_cov, 1) #sum over dimensions so its [B]
-
-    #     all_ = term1 + term2 + term3
-    #     log_norm = -.5 * all_
-
-    #     return log_norm
 
 
     def recognition_net(self, input_):
@@ -279,8 +237,6 @@ class DKF():
         output: elbo scalar
         '''
 
-        #TODO allow for multiple particles
-
         def fn_over_timesteps(particle_and_logprobs, xa_t):
             '''
             particle_and_logprobs: [B,PZ+3]
@@ -290,8 +246,6 @@ class DKF():
 
             return [B,Z+3]
             '''
-
-
 
             #unpack previous particle_and_logprobs, prev_z:[B,PZ], ignore prev logprobs
             prev_particles = tf.slice(particle_and_logprobs, [0,0], [self.batch_size, self.n_particles*self.z_size])
@@ -310,14 +264,10 @@ class DKF():
                 prev_z = tf.slice(prev_particles, [0,i*self.z_size], [self.batch_size, self.z_size])
 
                 #combine prez and current a (used for prior)
-                # prev_z_and_current_a = tf.concat(1, [prev_z, current_a]) #[B,ZA]
                 prev_z_and_current_a = tf.concat([prev_z, current_a], axis=1) #[B,ZA]
-
-
 
                 #ENCODE
                 #Concatenate current x, current action, prev_z: [B,XA+Z]
-                # concatenate_all = tf.concat(1, [xa_t, prev_z])
                 concatenate_all = tf.concat([xa_t, prev_z], axis=1)
 
                 #Predict q(z|z-1,u,x): [B,Z] [B,Z]
@@ -378,11 +328,7 @@ class DKF():
 
 
 
-
-
-
         # Put obs and actions to together [B,T,X+A]
-        # x_and_a = tf.concat(2, [x, actions])
         x_and_a = tf.concat([x, actions], axis=2)
 
         # Transpose so that timesteps is first [T,B,X+A]
@@ -397,12 +343,7 @@ class DKF():
 
 
         # Scan over timesteps, returning particles and logprobs [T,B,Z+3]
-        # print fn_over_timesteps
-        # print x_and_a
-        # print initializer
         self.particles_and_logprobs = tf.scan(fn_over_timesteps, x_and_a, initializer=initializer)
-
-
 
         #unpack the logprobs  list([pz+3,T,B])
         particles_and_logprobs_unstacked = tf.unstack(self.particles_and_logprobs, axis=2)
@@ -531,8 +472,6 @@ class DKF():
             # give last state and generate next states and obs
         #append gen to real 
 
-        #now im going to add multiple particles
-
 
         saver = tf.train.Saver()
         self.sess = tf.Session()
@@ -569,15 +508,10 @@ class DKF():
             
 
 
-
-
         #Get states [T,B,PZ+3]
         current_state = self.sess.run(self.particles_and_logprobs, feed_dict={self.x: given_sequence, self.actions: given_actions})
         # Unpack, get states
         current_state = current_state[n_time_steps_given-1][0][:self.z_size*self.n_particles]
-
-
-
 
 
         #Step 2: Predict future states and decode to frames
@@ -625,105 +559,6 @@ class DKF():
 
 
 
-    # def get_current_emission(self, state, prev_emission):
-
-
-    #     current_emission = self.sess.run(self.current_emission, feed_dict={self.current_z: state, self.prev_x: prev_emission})
-
-    #     return current_emission
-
-
-    # def get_next_state(self, prev_z, current_action, prev_x):
-
-
-    #     next_state = self.sess.run(self.next_state, feed_dict={self.prev_z: prev_z, self.prev_x: prev_x, self.current_action: current_action})
-
-    #     return next_state
-
-
-
-
-
-
-
-# if __name__ == "__main__":
-
-#     save_to = home + '/data/' #for boltz
-#     # save_to = home + '/Documents/tmp/' # for mac
-
-#     training_steps = 4000
-#     f_height=30
-#     f_width=30
-#     ball_size=5
-#     n_time_steps = 10
-#     n_particles = 3
-#     batch_size = 4
-#     path_to_load_variables=save_to + 'dkf_ball_vars.ckpt'
-#     # path_to_load_variables=''
-#     path_to_save_variables=save_to + 'dkf_ball_vars2.ckpt'
-#     # path_to_save_variables=''
-
-#     train = 1
-#     generate = 1
-
-
-#     def get_ball():
-#         return make_ball_gif(n_frames=n_time_steps, f_height=f_height, f_width=f_width, ball_size=ball_size, max_1=True, vector_output=True)
-
-
-#     network_architecture = \
-#         dict(   encoder_net=[100,100],
-#                 decoder_net=[100,100],
-#                 trans_net=[100,100],
-#                 n_input=f_height*f_width, # image as vector
-#                 n_z=20,  # dimensionality of latent space
-#                 n_actions=4) #4 possible actions
-    
-#     print 'Initializing model..'
-#     dkf = DKF(network_architecture, transfer_fct=tf.nn.softplus, learning_rate=0.001, batch_size=batch_size, n_time_steps=n_time_steps, n_particles=n_particles)
-
-
-#     if train:
-#         print 'Training'
-#         dkf.train(get_data=get_ball, steps=training_steps, display_step=20, path_to_load_variables=path_to_load_variables, path_to_save_variables=path_to_save_variables)
-
-
-#     #GENERATE - Give it a sequence of actions and make it generate the frames
-#     if generate:
-#         print 'Generating'
-        
-#         # real: [B,T,X] gen:[T,K,B,X]
-#         real_frames, gen_frames = dkf.run_generate(get_data=get_ball, path_to_load_variables=path_to_save_variables)
-
-
-#         real_gif = []
-#         gen_gif = []
-#         for t in range(n_time_steps):
-
-#             real_frame = real_frames[0][t]
-#             real_frame = real_frame * (255. / np.max(real_frame))
-#             real_frame = real_frame.astype('uint8')
-#             real_gif.append(np.reshape(real_frame, [f_height,f_width, 1]))
-
-#             gen_frame = gen_frames[t][0][0]
-#             gen_frame = gen_frame * (255. / np.max(gen_frame))
-#             gen_frame = gen_frame.astype('uint8')
-#             gen_gif.append(np.reshape(gen_frame, [f_height,f_width, 1]))
-
-
-#         real_gif = np.array(real_gif)
-#         gen_gif = np.array(gen_gif)
-
-
-#         kargs = { 'duration': .6 }
-#         imageio.mimsave(save_to+'gen_gif.gif', gen_gif, 'GIF', **kargs)
-#         print 'saved gif:' + save_to+'gen_gif.gif'
-
-#         kargs = { 'duration': .6 }
-#         imageio.mimsave(save_to+'real_gif.gif', real_gif, 'GIF', **kargs)
-#         print 'saved gif:' +save_to+'real_gif.gif'
-
-#     donedonedone
 
 
 
