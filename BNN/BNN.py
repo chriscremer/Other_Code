8a74fc1074e6361432e214c26674371cf7d53be7
Chris <chris.a.cremer@gmail.com>
2017-05-07 12:45:37 -0400

    completed BNN

--- BNN/BNN.py
+++ BNN/BNN.py
@@ -2,6 +2,7 @@
 #Bayesian Neural Network
 
 import numpy as np
+import numpy.random as npr
 import tensorflow as tf
 import random
 import math
@@ -22,71 +23,92 @@ class BNN(object):
         #Model hyperparameters
         self.act_func = tf.nn.softplus #tf.tanh
         self.learning_rate = .0001
-        self.batch_size = 20
-        self.n_particles = 3
         self.rs = 0
-        # self.n_input = network_architecture["n_input"]
+        self.input_size = network_architecture[0]
+        self.output_size = network_architecture[-1]
+        self.net = network_architecture
 
+        #Placeholders - Inputs/Targets [B,X]
+        self.batch_size = tf.placeholder(tf.int32, None)
+        self.n_particles = tf.placeholder(tf.int32, None)
+        self.batch_fraction_of_dataset = tf.placeholder(tf.float32, None)
+        self.x = tf.placeholder(tf.float32, [None, self.input_size])
+        self.y = tf.placeholder(tf.float32, [None, self.output_size])
 
-        #Model
-        self.model(network_architecture)
+        #Feedforward: [B,P,Y], [P], [P]
+        y_hat, log_p_W, log_q_W = self.model(self.x)
 
+        #Likelihood [B,P]
+        log_p_y_hat = self.log_likelihood(self.y, y_hat)
 
-        
-        # # #Variables
-        # # self.network_weights, self.n_decoder_weights = self._initialize_weights(network_architecture)
+        #Objective
+        self.elbo = self.objective(log_p_y_hat, log_p_W, log_q_W, self.batch_fraction_of_dataset)
 
-        # #Sample weights
-        # self.sampled_theta, log_p_theta_, log_q_theta_ = self.sample_weights(self.network_weights['decoder_weights'])
+        # Minimize negative ELBO
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, epsilon=1e-02).minimize(-self.elbo)
 
-        # #Encoder - Recognition model - q(z|x): recog_mean,z_log_std_sq=[batch_size, n_z]
-        # self.recog_means, self.recog_log_vars = self._recognition_network(self.x, self.network_weights['encoder_weights']) #, self.network_weights['encoder_biases'])
-        
-        # #Sample z
-        # eps = tf.random_normal((self.n_particles, self.batch_size, self.n_z), 0, 1, dtype=tf.float32)
-        # self.z = tf.add(self.recog_means, tf.multiply(tf.sqrt(tf.exp(self.recog_log_vars)), eps)) #uses broadcasting, z=[n_parts, n_batches, n_z]
-        
-        # #Decoder - Generative model - p(x|z)
-        # self.x_reconstr_mean_no_sigmoid = self._generator_network(self.z, self.sampled_theta)#, self.network_weights['decoder_biases']) #no sigmoid
+        #For evaluation
+        self.prediction = tf.nn.softmax(y_hat) 
 
-        # #Objective
-        # self.elbo = self.elbo(self.x, self.x_reconstr_mean_no_sigmoid, self.z, self.recog_means, self.recog_log_vars, log_p_theta_, log_q_theta_)
+        #To init variables
+        self.init_vars = tf.global_variables_initializer()
 
-        # # Use ADAM optimizer
-        # self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, epsilon=1e-02).minimize(-self.elbo)
-
-        # #For evaluation
-        # self.log_w = self._log_likelihood(self.x, self.x_reconstr_mean_no_sigmoid) + self._log_p_z(self.z) - self._log_q_z_given_x(self.z, self.recog_means, self.recog_log_vars)
-        # self.x_reconstr_mean = tf.nn.sigmoid(self.x_reconstr_mean_no_sigmoid)
-
-
+        #For loadind/saving variables
+        self.saver = tf.train.Saver()
 
         #to make sure im not adding nodes to the graph
         tf.get_default_graph().finalize()
+
+        #Start session
+        self.sess = tf.Session()
 
 
     def log_normal(self, position, mean, log_var):
         '''
         Log of normal distribution
-        position is [P, B, D]
-        mean is [B, D]
-        log_var is [B, D]
-        output is [P, B]
+        position is [P, D]
+        mean is [D]
+        log_var is [D]
+        output is [P]
         '''
 
-        n_D = tf.shape(mean)[1]
+        n_D = tf.to_float(tf.shape(mean)[0])
         term1 = n_D * tf.log(2*math.pi)
-        term2 = tf.reduce_sum(log_var, reduction_indices=1) #sum over D,[B]
+        term2 = tf.reduce_sum(log_var, 0) #sum over D [1]
         dif_cov = tf.square(position - mean) / tf.exp(log_var)
-        term3 = tf.reduce_sum(dif_cov, 2) #sum over D, [P, B]
+        term3 = tf.reduce_sum(dif_cov, 1) #sum over D [P]
         all_ = term1 + term2 + term3
         log_normal_ = -.5 * all_
 
         return log_normal_
 
 
+    def log_likelihood(self, y, y_hat):
+        '''
+        Calculate p(y|x,theta)
+        y: [B,Y]
+        y_hat: [B,P,Y]
+        '''
 
-    def model(self, net):
+        #tile y for each particle [B,P,Y]
+        y = tf.tile(y, [1,self.n_particles])
+        y = tf.reshape(y, [self.batch_size,self.n_particles,self.output_size])
+
+        # [B,P]
+        log_likelihood = tf.nn.softmax_cross_entropy_with_logits(logits=y_hat, labels=y)
+
+        return -log_likelihood
+
+
+    def model(self, x):
+        '''
+        x: [B,X]
+        y_hat: [B,P,O]
+        log_p_W: [P]
+        log_q_W: [P]
+        '''
+
+        net = self.net
 
         def xavier_init(fan_in, fan_out, constant=1): 
             """ Xavier initialization of network weights"""
@@ -95,11 +117,12 @@ class BNN(object):
             high = constant*np.sqrt(6.0/(fan_in + fan_out))
             return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
 
+        #[B,X]
+        cur_val = x
+        #tile x for each particle [B,P,X]
+        cur_val = tf.tile(cur_val, [1,self.n_particles])
+        cur_val = tf.reshape(cur_val, [self.batch_size,self.n_particles,self.input_size])
 
-        #Placeholders - Inputs [B,X]
-        self.x = tf.placeholder(tf.float32, [None, net[0]])
-
-        cur_val = self.x
 
         log_p_W_sum = 0
         log_q_W_sum = 0
@@ -107,37 +130,165 @@ class BNN(object):
         for layer_i in range(len(net)-1):
 
             input_size_i = net[layer_i]+1 #plus 1 for bias
-            output_size_i = net[layer_i+1]
+            output_size_i = net[layer_i+1] #plus 1 because we want layer i+1
 
-            #Define variables
+            #Define variables [IS,OS]
             W_means = tf.Variable(xavier_init(input_size_i, output_size_i))
             W_logvars = tf.Variable(xavier_init(input_size_i, output_size_i))
 
-            #Sample weights [B,P,I,O]
+            #Sample weights [IS,OS]*[P,IS,OS]=[P,IS,OS]
             eps = tf.random_normal((self.n_particles, input_size_i, output_size_i), 0, 1, seed=self.rs)
             W = tf.add(W_means, tf.multiply(tf.sqrt(tf.exp(W_logvars)), eps))
 
-            # #Compute probs of samples 
-            # log_p_W_sum += tf.reduce_sum(self.log_normal(W, tf.zeros([self.batch_size,self.n_particles,]), tf.ones()))
-            # log_q_W_sum += tf.reduce_sum(self.log_normal(W, tf.reshape(W_means, [self.batch_size,], W_logvars)))
+            #Compute probs of samples  [B,P,1]
+            flat_w = tf.reshape(W,[self.n_particles,input_size_i*output_size_i]) #[P,IS*OS]
+            flat_W_means = tf.reshape(W_means, [input_size_i*output_size_i]) #[IS*OS]
+            flat_W_logvars = tf.reshape(W_logvars, [input_size_i*output_size_i]) #[IS*OS]
+            log_p_W_sum += self.log_normal(flat_w, tf.zeros([input_size_i*output_size_i]), tf.log(tf.ones([input_size_i*output_size_i])))
+            log_q_W_sum += self.log_normal(flat_w, flat_W_means, flat_W_logvars)
 
-        #     #Concat 1 to input for biases
-        #     cur_val = tf.concat([cur_val,tf.ones([self.n_particles*self.batch_size,1])], axis=1)
+            #Concat 1 to input for biases  [B,P,X]->[B,P,X+1]
+            cur_val = tf.concat([cur_val,tf.ones([self.batch_size,self.n_particles, 1])], axis=2)
+            #[B,P,X]->[B,P,1,X]
+            cur_val = tf.reshape(cur_val, [self.batch_size, self.n_particles, 1, input_size_i])
+            #[P,X,X']->[B,P,X,X']
+            W = tf.tile(W, [self.batch_size,1,1])
+            W = tf.reshape(W, [self.batch_size, self.n_particles, input_size_i, output_size_i])
 
-        #     #Forward Propagate [B,P,O]
-        #     cur_val = self.transfer_fct(tf.matmul(z, W))
+            #Forward Propagate  [B,P,X]*[P,X,X']->[B,P,X']
+            #Forward Propagate  [B,P,1,X]*[B,P,X,X']->[B,P,1,X']
+            if layer_i != len(net)-2:
+                cur_val = self.act_func(tf.matmul(cur_val, W))
+            else:
+                cur_val = tf.matmul(cur_val, W)
 
-        # return cur_val, log_p_W_sum, log_q_W_sum
+            #[B,P,1,X']->[B,P,X']
+            cur_val = tf.reshape(cur_val, [self.batch_size,self.n_particles,output_size_i])
+
+        return cur_val, log_p_W_sum, log_q_W_sum
+
+
+    def objective(self, logpy, logpW, logqW, batch_frac):
+        '''
+        logpy:[B,P]
+        logpW:[P]
+        logqW:[P]
+        '''
+
+        self.logpy = tf.reduce_mean(logpy)
+        self.logpW = tf.reduce_mean(logpW)
+        self.logqW = tf.reduce_mean(logqW)
+
+        # #Average over particles [B]
+        # elbo = tf.reduce_mean(logpy + (batch_frac*logpW) - logqW, axis=1)
+        # #Average over batch [1]
+        # elbo = tf.reduce_mean(elbo, axis=0)
+
+        elbo = self.logpy + batch_frac*(self.logpW - self.logqW)
+
+        return elbo
+
+
+
+
+    def train(self, train_x, train_y, valid_x=[], valid_y=[], display_step=5, path_to_load_variables='', path_to_save_variables='', epochs=10, batch_size=20, n_particles=3):
+        '''
+        Train.
+        '''
+        random_seed=0
+        rs=npr.RandomState(random_seed)
+        n_datapoints = len(train_y)
+        arr = np.arange(n_datapoints)
+
+        if path_to_load_variables == '':
+            self.sess.run(self.init_vars)
+
+        else:
+            #Load variables
+            self.saver.restore(self.sess, path_to_load_variables)
+            print 'loaded variables ' + path_to_load_variables
+
+        #start = time.time()
+        for epoch in range(epochs):
+
+            #shuffle the data
+            rs.shuffle(arr)
+            train_x = train_x[arr]
+            train_y = train_y[arr]
+
+            data_index = 0
+            for step in range(n_datapoints/batch_size):
+
+                #Make batch
+                batch = []
+                batch_y = []
+                while len(batch) != batch_size:
+                    batch.append(train_x[data_index]) 
+                    one_hot=np.zeros(10)
+                    one_hot[train_y[data_index]]=1.
+                    batch_y.append(one_hot)
+                    data_index +=1
+
+                # Fit training using batch data
+                _ = self.sess.run((self.optimizer), feed_dict={self.x: batch, self.y: batch_y, 
+                                                        self.batch_size: batch_size,
+                                                        self.n_particles: n_particles, 
+                                                        self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+
+                # Display logs per epoch step
+                if step % display_step == 0:
+
+                    cost,logpy,logpW,logqW,pred = self.sess.run((self.elbo,self.logpy, self.logpW, self.logqW,self.prediction), feed_dict={self.x: batch, self.y: batch_y, 
+                                                        self.batch_size: batch_size, 
+                                                        self.n_particles:n_particles, 
+                                                        self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+                    # cost = -cost #because I want to see the NLL
+                    print "Epoch", str(epoch+1)+'/'+str(epochs), 'Step:%04d' % (step+1) +'/'+ str(n_datapoints/batch_size), "elbo=", "{:.6f}".format(float(cost)),logpy,logpW,logqW #, 'time', time.time() - start
+                    print 'target'
+                    print batch_y[0]
+                    print 'prediciton'
+                    # print pred.shape
+                    print pred[0][0]
+                    print
+
+        if path_to_save_variables != '':
+            self.saver.save(self.sess, path_to_save_variables)
+            print 'Saved variables to ' + path_to_save_variables
+
+
+
+
 
 
 
 
 if __name__ == '__main__':
 
-    net = [784,200,10] 
+    net = [784,20,10] 
 
     model = BNN(net)
-    
+
+    print 'Loading data'
+    with open(home+'/Documents/MNIST_data/mnist.pkl','rb') as f:
+        mnist_data = pickle.load(f)
+
+    train_x = mnist_data[0][0]
+    train_y = mnist_data[0][1]
+    valid_x = mnist_data[1][0]
+    valid_y = mnist_data[1][1]
+    test_x = mnist_data[2][0]
+    test_y = mnist_data[2][1]
+
+    print 'Training'
+    model.train(train_x=train_x, train_y=train_y, epochs=10, batch_size=50, n_particles=2, display_step=100)
+
+    #LOOK AT PREDICTION DIFFERNECE BETWEEN 20 vs 200 hidden layer size . why is error so different
+        #because 200 has more extreme values, like close to 0
+
+
+    # is the cross entropy the correct log likelihood??
+
+
     print 'Done.'
 
 
