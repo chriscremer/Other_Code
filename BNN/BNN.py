2941135039b418395c766b172c505ea511421b0d
chriscremer <chris.a.cremer@gmail.com>
2017-05-07 23:40:25 -0400

    trying to fix bug

--- BNN/BNN.py
+++ BNN/BNN.py
@@ -56,6 +56,10 @@ class BNN(object):
         #For loadind/saving variables
         self.saver = tf.train.Saver()
 
+        #For debugging 
+        self.vars = tf.trainable_variables()
+        self.grads = tf.gradients(self.elbo, tf.trainable_variables())
+
         #to make sure im not adding nodes to the graph
         tf.get_default_graph().finalize()
 
@@ -89,13 +93,13 @@ class BNN(object):
         y: [B,Y]
         y_hat: [B,P,Y]
         '''
-
+        self.y1 = y
         #tile y for each particle [B,P,Y]
-        y = tf.tile(y, [1,self.n_particles])
-        y = tf.reshape(y, [self.batch_size,self.n_particles,self.output_size])
+        self.y2 = tf.tile(self.y1, [1,self.n_particles])
+        self.y3 = tf.reshape(self.y2, [self.batch_size,self.n_particles,self.output_size])
 
         # [B,P]
-        log_likelihood = tf.nn.softmax_cross_entropy_with_logits(logits=y_hat, labels=y)
+        log_likelihood = tf.nn.softmax_cross_entropy_with_logits(logits=y_hat, labels=self.y3)
 
         return -log_likelihood
 
@@ -140,12 +144,19 @@ class BNN(object):
             eps = tf.random_normal((self.n_particles, input_size_i, output_size_i), 0, 1, seed=self.rs)
             W = tf.add(W_means, tf.multiply(tf.sqrt(tf.exp(W_logvars)), eps))
 
+            # #debug
+            # W = tf.tile(W_means, [self.n_particles, 1])
+            # W = tf.reshape(W, [self.n_particles, input_size_i, output_size_i])
+
             #Compute probs of samples  [B,P,1]
             flat_w = tf.reshape(W,[self.n_particles,input_size_i*output_size_i]) #[P,IS*OS]
             flat_W_means = tf.reshape(W_means, [input_size_i*output_size_i]) #[IS*OS]
             flat_W_logvars = tf.reshape(W_logvars, [input_size_i*output_size_i]) #[IS*OS]
             log_p_W_sum += self.log_normal(flat_w, tf.zeros([input_size_i*output_size_i]), tf.log(tf.ones([input_size_i*output_size_i])))
             log_q_W_sum += self.log_normal(flat_w, flat_W_means, flat_W_logvars)
+
+            if layer_i==0:
+                self.debug_pw = self.log_normal(flat_w, tf.zeros([input_size_i*output_size_i]), tf.log(tf.ones([input_size_i*output_size_i])))
 
             #Concat 1 to input for biases  [B,P,X]->[B,P,X+1]
             cur_val = tf.concat([cur_val,tf.ones([self.batch_size,self.n_particles, 1])], axis=2)
@@ -155,7 +166,6 @@ class BNN(object):
             W = tf.tile(W, [self.batch_size,1,1])
             W = tf.reshape(W, [self.batch_size, self.n_particles, input_size_i, output_size_i])
 
-            #Forward Propagate  [B,P,X]*[P,X,X']->[B,P,X']
             #Forward Propagate  [B,P,1,X]*[B,P,X,X']->[B,P,1,X']
             if layer_i != len(net)-2:
                 cur_val = self.act_func(tf.matmul(cur_val, W))
@@ -195,7 +205,7 @@ class BNN(object):
         '''
         Train.
         '''
-        random_seed=0
+        random_seed=1
         rs=npr.RandomState(random_seed)
         n_datapoints = len(train_y)
         arr = np.arange(n_datapoints)
@@ -248,8 +258,59 @@ class BNN(object):
                     print ["{:.2f}".format(float(x)) for x in batch_y[0]] 
                     # print 'prediciton'
                     # print pred.shape
-                    print ["{:.2f}".format(float(x)) for x in pred[0][0]]  
+                    print ["{:.2f}".format(float(x)) for x in pred[0][0]] 
                     print
+
+
+                    # debugpw = self.sess.run((self.debug_pw), feed_dict={self.x: batch, self.y: batch_y, 
+                    #                                     self.batch_size: batch_size, 
+                    #                                     self.n_particles:n_particles, 
+                    #                                     self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+                    # print debugpw
+
+
+                    grads = self.sess.run((self.grads), feed_dict={self.x: batch, self.y: batch_y, 
+                                                        self.batch_size: batch_size, 
+                                                        self.n_particles:n_particles, 
+                                                        self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+                    print len(grads)
+                    print grads[0].shape
+                    print grads[0]
+                    # print vars_[0]
+                    print grads[1].shape
+                    print grads[1]
+                    print grads[2].shape
+                    print grads[2]
+                    print grads[3].shape
+                    print grads[3]
+
+
+                    # vars_ = self.sess.run((self.vars), feed_dict={self.x: batch, self.y: batch_y, 
+                    #                                     self.batch_size: batch_size, 
+                    #                                     self.n_particles:n_particles, 
+                    #                                     self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+                    # print len(vars_)
+                    # print vars_[0].shape
+                    # # print vars_[0]
+                    # print vars_[1].shape
+                    # print vars_[2].shape
+                    # print vars_[2]
+                    # print vars_[3].shape
+                    
+
+                    # y1,y2,y3 = self.sess.run((self.y1,self.y2,self.y3 ), feed_dict={self.x: batch, self.y: batch_y, 
+                    #                                     self.batch_size: batch_size, 
+                    #                                     self.n_particles:n_particles, 
+                    #                                     self.batch_fraction_of_dataset: batch_size/float(n_datapoints)})
+                    # print y1.shape
+                    # for b in range(len(y1)):
+                    #     print y1[b]
+                    # print y2.shape
+                    # print y3.shape
+                    # for b in range(len(y3)):
+                    #     print y3[b]
+                    # print 
+                    # fasd
 
         if path_to_save_variables != '':
             self.saver.save(self.sess, path_to_save_variables)
@@ -279,23 +340,15 @@ if __name__ == '__main__':
     test_x = mnist_data[2][0]
     test_y = mnist_data[2][1]
 
-    path_to_save_variables=home+'/Documents/tmp/vars.ckpt'
-    path_to_load_variables=home+'/Documents/tmp/vars.ckpt'
+    path_to_load_variables=home+'/Documents/tmp/vars.ckpt' 
     # path_to_load_variables=''
-
+    path_to_save_variables=home+'/Documents/tmp/vars2.ckpt'
 
     print 'Training'
     model.train(train_x=train_x, train_y=train_y, 
-                epochs=10, batch_size=20, n_particles=2, display_step=1000,
+                epochs=50, batch_size=20, n_particles=2, display_step=1000,
                 path_to_load_variables=path_to_load_variables,
                 path_to_save_variables=path_to_save_variables)
-
-    #LOOK AT PREDICTION DIFFERNECE BETWEEN 20 vs 200 hidden layer size . why is error so different
-        #because 200 has more extreme values, like close to 0
-
-
-    # is the cross entropy the correct log likelihood??
-        #yes
 
 
     print 'Done.'
