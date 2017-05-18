0cfafdfe7377ab1eefcb39f0378c0fdc06b6e7b0
Chris <chris.a.cremer@gmail.com>
2017-05-18 14:50:20 -0400

    bvae vs vae

---
+++ BVAE/BVAE_vs_VAE/BVAE.py
@@ -0,0 +1,479 @@
+
+# Bayesian Variational Autoencoder
+
+
+
+import numpy as np
+import tensorflow as tf
+import pickle
+from os.path import expanduser
+home = expanduser("~")
+
+from utils import log_normal as log_norm
+from utils import log_bernoulli as log_bern
+
+from NN import NN
+from BNN import BNN
+
+
+class BVAE(object):
+
+
+    def __init__(self, hyperparams):
+
+        tf.reset_default_graph()
+
+        #Model hyperparameters
+        self.learning_rate = hyperparams['learning_rate']
+        self.encoder_act_func = tf.nn.elu #tf.nn.softplus #tf.tanh
+        self.decoder_act_func = tf.tanh
+        self.encoder_net = hyperparams['encoder_net']
+        self.decoder_net = hyperparams['decoder_net']
+        self.z_size = hyperparams['z_size']  #Z
+        self.x_size = hyperparams['x_size']  #X
+        self.rs = 0
+        self.n_W_particles = hyperparams['n_W_particles']  #S
+        self.n_z_particles = hyperparams['n_z_particles']  #P
+
+        #Placeholders - Inputs/Targets
+        self.x = tf.placeholder(tf.float32, [None, self.x_size])
+        self.batch_size = tf.shape(self.x)[0]   #B
+        self.batch_frac = tf.placeholder(tf.float32, None)
+        
+
+        #Define endocer and decoder
+        with tf.variable_scope("encoder"):
+            encoder = NN(self.encoder_net, self.encoder_act_func, self.batch_size)
+
+        with tf.variable_scope("decoder"):
+            decoder = BNN(self.decoder_net, self.decoder_act_func, self.batch_size)
+        
+
+        #Objective
+        log_probs = self.log_probs(self.x, encoder, decoder)
+
+        self.elbo = self.objective(*log_probs)
+        self.iwae_elbo = self.iwae_objective(*log_probs)
+
+        self.iwae_elbo_test = self.iwae_objective_test(*log_probs)
+
+        # Minimize negative ELBO
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, 
+                                                epsilon=1e-02).minimize(-self.elbo)
+
+
+        # for var in tf.global_variables():
+        #     print var
+
+
+        #Finalize Initilization
+        self.init_vars = tf.global_variables_initializer()
+        self.saver = tf.train.Saver()
+        tf.get_default_graph().finalize()
+        # self.sess = tf.Session()
+
+
+    def log_probs(self, x, encoder, decoder):
+
+        log_px_list = []
+        log_pz_list = []
+        log_qz_list = []
+        log_pW_list = []
+        log_qW_list = []
+
+        for W_i in range(self.n_W_particles):
+
+            # Sample decoder weights  __, [1], [1]
+            W, log_pW, log_qW = decoder.sample_weights()
+
+            # Sample z   [P,B,Z], [P,B], [P,B]
+            z, log_pz, log_qz = self.sample_z(x, encoder, decoder, W)
+            # z: [PB,Z]
+            z = tf.reshape(z, [self.n_z_particles*self.batch_size, self.z_size])
+
+            # Decode [PB,X]
+            y = decoder.feedforward(W, z)
+            # y: [P,B,X]
+            y = tf.reshape(y, [self.n_z_particles, self.batch_size, self.x_size])
+
+            # Likelihood p(x|z)  [P,B]
+            log_px = log_bern(x,y)
+
+            #Store for later
+            log_px_list.append(log_px)
+            log_pz_list.append(log_pz)
+            log_qz_list.append(log_qz)
+            log_pW_list.append(log_pW)
+            log_qW_list.append(log_qW)
+
+
+        log_px = tf.stack(log_px_list) #[S,P,B]
+        log_pz = tf.stack(log_pz_list) #[S,P,B]
+        log_qz = tf.stack(log_qz_list) #[S,P,B]
+        log_pW = tf.stack(log_pW_list) #[S]
+        log_qW = tf.stack(log_qW_list) #[S]
+
+        return [log_px, log_pz, log_qz, log_pW, log_qW]  
+
+
+
+    def objective(self, log_px, log_pz, log_qz, log_pW, log_qW):
+        '''
+        log_px, log_pz, log_qz: [S,P,B]
+        log_pW, log_qW: [S]
+        Output: [1]
+        '''
+
+        # Calculte log probs for printing
+        self.log_px = tf.reduce_mean(log_px)
+        self.log_pz = tf.reduce_mean(log_pz)
+        self.log_qz = tf.reduce_mean(log_qz)
+        self.log_pW = tf.reduce_mean(log_pW)
+        self.log_qW = tf.reduce_mean(log_qW)
+        self.z_elbo = self.log_px + self.log_pz - self.log_qz 
+
+        #Calc elbo
+        elbo = self.log_px + self.log_pz - self.log_qz + self.batch_frac*(self.log_pW - self.log_qW)
+
+        return elbo
+
+
+
+    def iwae_objective(self, log_px, log_pz, log_qz, log_pW, log_qW):
+        '''
+        log_px, log_pz, log_qz: [S,P,B]
+        log_pW, log_qW: [S]
+        Output: [1]
+        '''
+
+        # Log mean exp over S and P, mean over B
+        temp_elbo = tf.reduce_mean(log_px + log_pz - log_qz, axis=2)   #[S,P]
+        log_pW = tf.reshape(log_pW, [self.n_W_particles, 1]) #[S,1]
+        log_qW = tf.reshape(log_qW, [self.n_W_particles, 1]) #[S,1]
+        temp_elbo = temp_elbo + (self.batch_frac*(log_pW - log_qW)) #broadcast, [S,P]
+        temp_elbo = tf.reshape(temp_elbo, [self.n_W_particles*self.n_z_particles]) #[SP]
+        max_ = tf.reduce_max(temp_elbo, axis=0) #[1]
+        iwae_elbo = tf.log(tf.reduce_mean(tf.exp(temp_elbo-max_))) + max_  #[1]
+
+        return iwae_elbo
+
+
+
+    # def objective_test(self, log_px, log_pz, log_qz, log_pW, log_qW):
+    #     '''
+    #     Uses approximate posterior
+    #     log_px, log_pz, log_qz: [S,P,B]
+    #     log_pW, log_qW: [S]
+    #     Output: [1]
+    #     '''
+
+    #     # Calculte log probs for printing
+    #     log_px = tf.reduce_mean(log_px)
+    #     log_pz = tf.reduce_mean(log_pz)
+    #     log_qz = tf.reduce_mean(log_qz)
+    #     # self.log_pW = tf.reduce_mean(log_pW)
+    #     # self.log_qW = tf.reduce_mean(log_qW)
+    #     # self.z_elbo = self.log_px + self.log_pz - self.log_qz 
+
+    #     #Calc elbo
+    #     elbo = self.log_px + self.log_pz - self.log_qz# + self.batch_frac*(self.log_pW - self.log_qW)
+
+    #     return elbo
+
+
+
+    def iwae_objective_test(self, log_px, log_pz, log_qz, log_pW, log_qW):
+        '''
+        Uses approximate posterior
+        log_px, log_pz, log_qz: [S,P,B]
+        log_pW, log_qW: [S]
+        Output: [1]
+        '''
+
+        # Log mean exp over S and P, mean over B
+        temp_elbo = tf.reduce_mean(log_px + log_pz - log_qz, axis=2)   #[S,P]
+        # log_pW = tf.reshape(log_pW, [self.n_W_particles, 1]) #[S,1]
+        # log_qW = tf.reshape(log_qW, [self.n_W_particles, 1]) #[S,1]
+        # temp_elbo = temp_elbo #+ (self.batch_frac*(log_pW - log_qW)) #broadcast, [S,P]
+        temp_elbo = tf.reshape(temp_elbo, [self.n_W_particles*self.n_z_particles]) #[SP]
+        max_ = tf.reduce_max(temp_elbo, axis=0) #[1]
+        iwae_elbo = tf.log(tf.reduce_mean(tf.exp(temp_elbo-max_))) + max_  #[1]
+
+        return iwae_elbo
+
+
+
+
+
+    def sample_z(self, x, encoder, decoder, W):
+        '''
+        z: [P,B,Z]
+        log_pz: [P,B]
+        log_qz: [P,B]
+        '''
+
+        #Encode
+        z_mean_logvar = encoder.feedforward(x) #[B,Z*2]
+        z_mean = tf.slice(z_mean_logvar, [0,0], [self.batch_size, self.z_size]) #[B,Z] 
+        z_logvar = tf.slice(z_mean_logvar, [0,self.z_size], [self.batch_size, self.z_size]) #[B,Z]
+
+        #Sample z  [P,B,Z]
+        eps = tf.random_normal((self.n_z_particles, self.batch_size, self.z_size), 0, 1, seed=self.rs) 
+        z = tf.add(z_mean, tf.multiply(tf.sqrt(tf.exp(z_logvar)), eps)) #broadcast, [P,B,Z]
+
+        # Calc log probs [P,B]
+        log_pz = log_norm(z, tf.zeros([self.batch_size, self.z_size]), 
+                                tf.log(tf.ones([self.batch_size, self.z_size])))
+        log_qz = log_norm(z, z_mean, z_logvar)
+
+        return z, log_pz, log_qz
+
+
+
+
+
+
+
+    def train(self, train_x, valid_x=[], display_step=5, 
+                path_to_load_variables='', path_to_save_variables='', 
+                epochs=10, batch_size=20):
+        '''
+        Train.
+        '''
+        with tf.Session() as self.sess:
+            # self.sess = tf.Session()
+            random_seed=1
+            rs=np.random.RandomState(random_seed)
+            n_datapoints = len(train_x)
+            arr = np.arange(n_datapoints)
+
+            if path_to_load_variables == '':
+                self.sess.run(self.init_vars)
+            else:
+                #Load variables
+                self.saver.restore(self.sess, path_to_load_variables)
+                print 'loaded variables ' + path_to_load_variables
+
+            #start = time.time()
+            for epoch in range(epochs):
+
+                #shuffle the data
+                rs.shuffle(arr)
+                train_x = train_x[arr]
+
+                data_index = 0
+                for step in range(n_datapoints/batch_size):
+                    #Make batch
+                    batch = []
+                    while len(batch) != batch_size:
+                        batch.append(train_x[data_index]) 
+                        data_index +=1
+                    # Fit training using batch data
+                    _ = self.sess.run((self.optimizer), feed_dict={self.x: batch, 
+                                                            self.batch_frac: 1./float(n_datapoints)})
+                    # Display logs per epoch step
+                    if step % display_step == 0:
+                        elbo,log_px,log_pz,log_qz,log_pW,log_qW, i_elbo = self.sess.run((self.elbo, 
+                                                                                    self.log_px, self.log_pz, 
+                                                                                    self.log_qz, self.log_pW, 
+                                                                                    self.log_qW, self.iwae_elbo), 
+                                                        feed_dict={self.x: batch, 
+                                                            self.batch_frac: 1./float(n_datapoints)})
+                        print ("Epoch", str(epoch+1)+'/'+str(epochs), 
+                                'Step:%04d' % (step+1) +'/'+ str(n_datapoints/batch_size), 
+                                "elbo={:.4f}".format(float(elbo)),
+                                log_px,log_pz,log_qz,log_pW,log_qW, i_elbo)
+
+            if path_to_save_variables != '':
+                self.saver.save(self.sess, path_to_save_variables)
+                print 'Saved variables to ' + path_to_save_variables
+
+
+
+
+    def eval(self, data, display_step=5, path_to_load_variables='',
+             batch_size=20, data2=[]):
+        '''
+        Evaluate.
+        '''
+        with tf.Session() as self.sess:
+
+            n_datapoints = len(data)
+            # n_datapoints_for_frac = len(data2)
+
+            if path_to_load_variables == '':
+                self.sess.run(self.init_vars)
+            else:
+                #Load variables
+                self.saver.restore(self.sess, path_to_load_variables)
+                print 'loaded variables ' + path_to_load_variables
+
+            iwae_elbos = []
+            logpxs=[]
+            logpzs=[]
+            logqzs=[]
+            logpWs=[]
+            logqWs=[]
+
+            data_index = 0
+            for step in range(n_datapoints/batch_size):
+
+                #Make batch
+                batch = []
+                while len(batch) != batch_size:
+                    batch.append(data[data_index]) 
+                    data_index +=1
+                # Calc iwae elbo on test set
+                iwae_elbo, log_px,log_pz,log_qz,log_pW,log_qW = self.sess.run((self.iwae_elbo_test,
+                                                                            self.log_px, self.log_pz, 
+                                                                            self.log_qz, self.log_pW, 
+                                                                            self.log_qW), 
+                                                        feed_dict={self.x: batch})
+                iwae_elbos.append(iwae_elbo)
+                logpxs.append(log_px)
+                logpzs.append(log_pz)
+                logqzs.append(log_qz)
+                logpWs.append(log_pW)
+                logqWs.append(log_qW)
+
+        return [np.mean(iwae_elbos), np.mean(logpxs), np.mean(logpzs), np.mean(logqzs), np.mean(logpWs), np.mean(logqWs)]
+
+
+
+
+
+
+# class BIWAE(BVAE):
+
+
+#     def __init__(self, hyperparams):
+
+
+#         tf.reset_default_graph()
+
+#         #Model hyperparameters
+#         self.learning_rate = hyperparams['learning_rate']
+#         self.encoder_act_func = tf.nn.elu #tf.nn.softplus #tf.tanh
+#         self.decoder_act_func = tf.tanh
+#         self.encoder_net = hyperparams['encoder_net']
+#         self.decoder_net = hyperparams['decoder_net']
+#         self.z_size = hyperparams['z_size']  #Z
+#         self.x_size = hyperparams['x_size']  #X
+#         self.rs = 0
+
+
+#         self.n_W_particles = hyperparams['n_W_particles']  #S
+#         # self.n_W_particles = tf.placeholder(tf.int32, None)  #S
+
+#         #Placeholders - Inputs/Targets
+#         self.x = tf.placeholder(tf.float32, [None, self.x_size])
+#         self.n_z_particles = tf.placeholder(tf.int32, None)  #P
+#         self.batch_frac = tf.placeholder(tf.float32, None)
+
+#         self.batch_size = tf.shape(self.x)[0]   #B
+
+#         #Define endocer and decoder
+#         with tf.variable_scope("encoder"):
+#             encoder = NN(self.encoder_net, self.encoder_act_func, self.batch_size)
+
+#         with tf.variable_scope("decoder"):
+#             decoder = BNN(self.decoder_net, self.decoder_act_func, self.batch_size)
+        
+#         #Objective
+#         log_probs = self.log_probs(self.x, encoder, decoder)
+
+#         self.elbo = self.iwae_objective(*log_probs)
+
+#         self.iwae_elbo = self.iwae_objective(*log_probs)
+
+#         #for printing
+#         stuff = self.objective(*log_probs)
+
+
+#         # Minimize negative ELBO
+#         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, 
+#                                                 epsilon=1e-02).minimize(-self.elbo)
+
+#         #Finalize Initilization
+#         self.init_vars = tf.global_variables_initializer()
+#         self.saver = tf.train.Saver()
+#         tf.get_default_graph().finalize()
+#         self.sess = tf.Session()
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    x_size = 784
+    z_size = 10
+
+    hyperparams = {
+        'learning_rate': .0001,
+        'x_size': x_size,
+        'z_size': z_size,
+        'encoder_net': [x_size, 20, z_size*2],
+        'decoder_net': [z_size, 20, x_size],
+        'n_W_particles': 1,
+        'n_z_particles': 1}
+
+    model = BVAE(hyperparams)
+
+    print 'Loading data'
+    with open(home+'/Documents/MNIST_data/mnist.pkl','rb') as f:
+        mnist_data = pickle.load(f)
+
+    train_x = mnist_data[0][0]
+    train_y = mnist_data[0][1]
+    valid_x = mnist_data[1][0]
+    valid_y = mnist_data[1][1]
+    test_x = mnist_data[2][0]
+    test_y = mnist_data[2][1]
+
+    # path_to_load_variables=home+'/Documents/tmp/vars.ckpt' 
+    path_to_load_variables=''
+    path_to_save_variables=home+'/Documents/tmp/vars2.ckpt'
+
+    print 'Training'
+    model.train(train_x=train_x,
+                epochs=1, batch_size=20, display_step=1000,
+                path_to_load_variables=path_to_load_variables,
+                path_to_save_variables=path_to_save_variables)
+
+
+
+    print 'Eval'
+    iwae_elbo = model.eval(data=test_x, batch_size=20, display_step=10,
+                path_to_load_variables=path_to_save_variables, data2=train_x)
+
+    print iwae_elbo
+
+    print 'Done.'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BVAE/BVAE_vs_VAE/experiments.py
@@ -0,0 +1,252 @@
+
+
+
+import numpy as np
+import tensorflow as tf
+import pickle, time, datetime
+from os.path import expanduser
+home = expanduser("~")
+# import sys
+# sys.path.insert(0, './BVAE_adding_eval_use_this')
+
+# import argparse
+# import os
+# from scipy.stats import multivariate_normal as norm
+
+from BVAE import BVAE
+from VAE import VAE
+
+
+
+
+
+def load_binarized_mnist(location):
+
+    with open(location, 'rb') as f:
+        train_x, valid_x, test_x = pickle.load(f)
+    return train_x, valid_x, test_x
+
+
+def load_mnist(location):
+
+    with open(location,'rb') as f:
+        mnist_data = pickle.load(f)
+    train_x = mnist_data[0][0]
+    train_y = mnist_data[0][1]
+    valid_x = mnist_data[1][0]
+    valid_y = mnist_data[1][1]
+    test_x = mnist_data[2][0]
+    test_y = mnist_data[2][1]
+    return train_x, valid_x, test_x
+
+
+
+if __name__ == '__main__':
+
+    # Paths
+    mnist_path = home+'/Documents/MNIST_data/mnist.pkl'
+    binarized_mnist_path = home+'/Documents/MNIST_data/binarized_mnist.pkl'
+    parameter_path = home+'/Documents/tmp/'
+    experiment_log_path = home+'/Documents/tmp/'
+
+    #Load data
+    train_x, valid_x, test_x = load_binarized_mnist(location=binarized_mnist_path)
+    print 'Train', train_x.shape
+    print 'Valid', valid_x.shape
+    print 'Test', test_x.shape
+
+
+    # Training settings
+    x_size = 784   #f_height=28f_width=28
+    n_batch = 20
+    epochs = 200
+    S_training = 1  #number of weight samples
+
+    #Experimental Variables
+    list_of_models = ['bvae', 'vae']
+    list_of_k_samples = [1]
+    z_sizes = [10,50,100]
+
+    # Test settings
+    S_evaluation = 1 #2
+    k_evaluation = 500 #500
+    n_batch_eval = 2 #2
+
+    #Experiment log
+    dt = datetime.datetime.now()
+    date_ = str(dt.date())
+    time_ = str(dt.time())
+    time_2 = time_[0] + time_[1] + time_[3] + time_[4] + time_[6] + time_[7] 
+    experiment_log = experiment_log_path+'experiment_' + date_ + '_' +time_2 +'.txt'
+    print 'Saving experiment log to ' + experiment_log
+
+
+    for k_training in list_of_k_samples:
+
+        for m in list_of_models:
+
+            for z_size in z_sizes:
+
+                saved_parameter_file = m + '_k' + str(k_training) + '_S'+str(S_training) + '_z'+str(z_size) + '_epochs'+str(epochs)+'.ckpt' 
+                print 'Current:', saved_parameter_file
+                with open(experiment_log, "a") as myfile:
+                    myfile.write('\nCurrent:' + saved_parameter_file +'\n')
+
+                #Train 
+                print 'Training'
+
+                hyperparams = {
+                    'learning_rate': .0001,
+                    'x_size': x_size,
+                    'z_size': z_size,
+                    'encoder_net': [x_size, 200, z_size*2],
+                    'decoder_net': [z_size, 200, x_size],
+                    'n_W_particles': S_training,
+                    'n_z_particles': k_training}
+
+                #Initialize model
+                if m == 'bvae':
+                    model = BVAE(hyperparams)
+                elif m == 'biwae':
+                    model = BIWAE(hyperparams)
+                elif m == 'vae':
+                    model = VAE(hyperparams)   
+
+                start = time.time()
+
+                model.train(train_x=train_x, valid_x=valid_x,
+                            epochs=epochs, batch_size=n_batch,
+                            display_step=3000,
+                            path_to_load_variables='',
+                            path_to_save_variables=parameter_path+saved_parameter_file)
+
+                time_to_train = time.time() - start
+                print 'Time to train', time_to_train
+                with open(experiment_log, "a") as f:
+                    f.write('Time to train '+  str(time_to_train) + '\n')
+
+
+
+
+
+
+
+
+                # #Initialize model
+                # if m == 'bvae':
+                #     model = BVAE(hyperparams)
+                # elif m == 'biwae':
+                #     model = BIWAE(hyperparams)
+
+                # start = time.time()
+
+                # model.train(train_x=train_x, valid_x=valid_x,
+                #             epochs=epochs, batch_size=n_batch, n_W_particles='notImplemented', n_z_particles=k_training, 
+                #             display_step=2400,
+                #             path_to_load_variables=parameter_path+saved_parameter_file,
+                #             path_to_save_variables=parameter_path+saved_parameter_file)
+
+                # time_to_train = time.time() - start
+                # print 'Time to train', time_to_train
+                # with open(experiment_log, "a") as f:
+                #     f.write('Time to train '+  str(time_to_train) + '\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+                #Evaluate
+                print 'Evaluating'
+
+                hyperparams = {
+                    'learning_rate': .0001,
+                    'x_size': x_size,
+                    'z_size': z_size,
+                    'encoder_net': [x_size, 200, z_size*2],
+                    'decoder_net': [z_size, 200, x_size],
+                    'n_W_particles': S_evaluation,
+                    'n_z_particles': k_evaluation}
+
+                #Initialize model
+                if m == 'bvae':
+                    model = BVAE(hyperparams)
+                elif m == 'biwae':
+                    model = BIWAE(hyperparams)            
+                elif m == 'vae':
+                    model = VAE(hyperparams)   
+
+                start = time.time()
+
+                info = model.eval(data=test_x, batch_size=n_batch_eval, n_W_particles='notImplemented', display_step=100,
+                                        path_to_load_variables=parameter_path+saved_parameter_file, data2=train_x)
+
+                time_to_eval = time.time() - start
+                print 'time to evaluate', time_to_eval
+                print 'Model Log Likelihood is ' + str(info) + ' for ' + saved_parameter_file
+                
+                with open(experiment_log, "a") as myfile:
+                    myfile.write('time to evaluate '+  str(time_to_eval) +'\n')
+                    myfile.write('Model Log Likelihood is ' + str(info) + ' for ' 
+                        + saved_parameter_file +'\n')
+                    myfile.write('z_elbo '+  str(info[2]+info[3]-info[4]) +'\n\n')
+                    
+
+                print 
+
+
+
+    #Examples: 
+    # python experiments.py -m vae -k 1 -a train -s vae_1
+    # python experiments.py -m vae -k 10 -a train -s vae_10_s6 -l vae_10 -ss 6 -es 6
+    # # python experiments.py -m vae -k 10 -a evaluate -l vae_10
+    # parser = argparse.ArgumentParser(description='Run experiments.')
+    # parser.add_argument('--model', '-m', choices=['vae', 'iwae', 'mog_vae', 'mog_iwae'], 
+    #     default='vae')
+    # parser.add_argument('--k', '-k', type=int, default=1)
+    # parser.add_argument('--action', '-a', choices=['train', 'evaluate', 'combined'], default='train')
+    # parser.add_argument('--save_to', '-s', type=str, default='')
+    # parser.add_argument('--load_from', '-l', type=str, default='')
+    # parser.add_argument('--starting_stage', '-ss', type=int, default=0)
+    # parser.add_argument('--ending_stage', '-es', type=int, default=5)
+    # parser.add_argument('--n_clusters', '-c', type=int, default=2)
+    # args = parser.parse_args()
+
+    # path_to_save_variables, path_to_load_variables = user_defined_locations(args)
+
+    # #Load data
+    # train_x, valid_x, test_x = load_binarized_mnist(location=home+'/data/binarized_mnist.pkl')
+    # print 'Train', train_x.shape
+    # print 'Valid', valid_x.shape
+    # print 'Test', test_x.shape
+
+
+
+        # Check for the model variables, if not there train it then eval. 
+        #Write results to a file in case something happens
+        #Also have a timer going to say how long each took. 
+
+
+
+    print 'All Done'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
