c6f1e031ac30fe874944ace7a33a74667b028e11
Chris <chris.a.cremer@gmail.com>
2017-07-11 23:41:22 -0400

    u

--- BVAE/BVAE_11jul2017/BVAE_2.py
+++ BVAE/BVAE_11jul2017/BVAE_2.py
@@ -485,12 +485,12 @@ class BVAE(object):
                         print ("Epoch", str(epoch+1)+'/'+str(epochs), 
                                 'Step:%04d' % (step+1) +'/'+ str(n_datapoints/batch_size), 
                                 "elbo={:.4f}".format(float(elbo)),
-                                '{:2f}'.format(logpx),
-                                '{:2f}'.format(logpz),
-                                '{:2f}'.format(logqz),
-                                '{:2f}'.format(logpW),
-                                '{:2f}'.format(logqW),
-                                '{:2f}'.format(l2_sum))
+                                '{:2f}'.format(float(logpx)),
+                                '{:2f}'.format(float(logpz)),
+                                '{:2f}'.format(float(logqz)),
+                                '{:2f}'.format(float(logpW)),
+                                '{:2f}'.format(float(logqW)),
+                                '{:2f}'.format(float(l2_sum)))
 
 
 
