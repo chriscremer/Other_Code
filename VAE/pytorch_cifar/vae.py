d7921a7cd57aa645bc0b7a4de07444ad0feb01cb
Chris <chris.a.cremer@gmail.com>
2017-09-07 14:49:57 -0400

    u

--- VAE/pytorch_cifar/vae.py
+++ VAE/pytorch_cifar/vae.py
@@ -673,9 +673,9 @@ class VAE(nn.Module):
 
 
         z = F.relu(self.deconv1(z))
-        print z.size()
+        # print z.size()
         z = self.conv2(z)
-        print z.size()
+        # print z.size()
 
         # z = nn.Upsample(size=[3,32,32])(z)
 
