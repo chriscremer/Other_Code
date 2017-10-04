de56b52a9036c395cef08f321d960e7a9d415309
chriscremer <chris.a.cremer@gmail.com>
2017-10-04 18:38:16 -0400

    add script

---
+++ RL_baselines/run.sh
@@ -0,0 +1,14 @@
+
+
+
+
+#Run
+# python -m baselines.deepq.experiments.atari.train --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2
+# python $HOME/Documents/baselines/baselines/deepq/experiments/atari/train.py --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2
+
+# in parens so dir doesnt change
+(cd $HOME/Documents/baselines/ && python -m baselines.deepq.experiments.atari.train --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2)
+
+
+
+
