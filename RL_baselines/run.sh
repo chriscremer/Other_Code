9ccee7dc1bb46775907a1b0ff1ac356873d2c30b
Chris <chris.a.cremer@gmail.com>
2017-10-05 13:35:48 -0400

    u

--- RL_baselines/run.sh
+++ RL_baselines/run.sh
@@ -6,9 +6,15 @@
 # python -m baselines.deepq.experiments.atari.train --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2
 # python $HOME/Documents/baselines/baselines/deepq/experiments/atari/train.py --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2
 
-# in parens so dir doesnt change
-(cd $HOME/Documents/baselines/ && python -m baselines.deepq.experiments.atari.train --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2)
 
+
+
+# in parens so dir doesnt change
+# (cd $HOME/Documents/baselines/ && python -m baselines.deepq.experiments.atari.train --env Pong --save-dir $HOME/Documents/tmp/baselines_pong2)
+
+
+# to see it play
+(cd $HOME/Documents/baselines/ && python -m baselines.deepq.experiments.atari.enjoy --env Pong --model-dir $HOME/Documents/tmp/baselines_pong2/model-1000000)
 
 
 
