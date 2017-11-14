302f4746f02ad1906be49f42175e4e2df4aa7f7e
chriscremer <chris.a.cremer@gmail.com>
2017-11-14 11:55:23 -0500

    refactored

--- RL4/rl_nov2017/viz_action_and_value_dists.py
+++ RL4/rl_nov2017/viz_action_and_value_dists.py
@@ -95,43 +95,6 @@ def viz(model_dict):
             current_state *= masks   #restart the done ones, by setting the state to zero
         return reward, masks, final_rewards, episode_rewards, current_state
 
-    # def do_vid():
-    #     n_vids=3
-    #     for i in range(n_vids):
-    #         done=False
-    #         state = envs_video.reset()
-    #         # state = torch.from_numpy(state).float().type(dtype)
-    #         current_state = torch.zeros(1, *obs_shape)
-    #         current_state = update_current_state(current_state, state, shape_dim0).type(dtype)
-    #         # print ('Recording')
-    #         # count=0
-    #         while not done:
-    #             # print (count)
-    #             # count +=1
-    #             # Act
-    #             state_var = Variable(current_state, volatile=True) 
-    #             # print (state_var.size())
-    #             action, value = agent.act(state_var)
-    #             cpu_actions = action.data.squeeze(1).cpu().numpy()
-
-    #             # Observe reward and next state
-    #             state, reward, done, info = envs_video.step(cpu_actions) # state:[nProcesss, ndims, height, width]
-    #             # state = torch.from_numpy(state).float().type(dtype)
-    #             # current_state = torch.zeros(1, *obs_shape)
-    #             current_state = update_current_state(current_state, state, shape_dim0).type(dtype)
-    #     state = envs_video.reset()
-        
-    #     vid_path = save_dir+'/videos/'
-    #     count =0
-    #     for aaa in os.listdir(vid_path):
-
-    #         if 'openaigym' in aaa and '.mp4' in aaa:
-    #             #os.rename(vid_path+aaa, vid_path+'vid_t'+str(total_num_steps)+'.mp4')
-    #             subprocess.call("(cd "+vid_path+" && mv "+ vid_path+aaa +" "+ vid_path+env_name+'_'+algo+'_vid_t'+str(total_num_steps)+'_'+str(count) +".mp4)", shell=True) 
-    #             count+=1
-    #         if '.json' in aaa:
-    #             os.remove(vid_path+aaa)
-
 
 
 
@@ -455,6 +418,79 @@ if __name__ == "__main__":
     frame_path = model_dict['save_to']+'/frames_'+ model_name_+ '_'+env_name_+ '_'+ str(int(epoch_level)) +'/'
 
     viz(model_dict)
+
+
+
+
+
+    dfadf
+    #from make_gif.py
+
+
+    import imageio
+
+
+    import os
+    from os.path import expanduser
+    home = expanduser("~")
+
+
+    # dir_ = home+ '/Downloads/frames_a2c_dropout_6000000'
+    dir_ = home+ '/Documents/tmp/a2c_reg_and_dropout_pong2/PongNoFrameskip-v4/a2c_dropout/seed0/frames_a2c_dropout_PongNoFrameskip-v4_6000000'
+
+
+    print('making gif')
+    max_epoch = 0
+    for file_ in os.listdir(dir_):
+        if 'plt' in file_:
+            numb = file_.split('plt')[1].split('.')[0]
+            numb = int(numb)
+            if numb > max_epoch:
+                max_epoch = numb
+            # print (numb)
+            # fadsfa
+
+    # print ('max_epoch', max_epoch)
+    # fadad
+
+
+    images = []
+    # for file_ in os.listdir(dir_):
+    for i in range(max_epoch+1):
+        # print(file_)
+        # fsdfa
+
+        # images.append(imageio.imread(dir_+'/'+file_))
+        images.append(imageio.imread(dir_+'/'+'plt'+str(i)+'.png'))
+
+        
+    imageio.mimsave(dir_+'/movie.gif', images)
+    print ('made gif', dir_+'/movie.gif')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ RL4/rl_nov2017_2/experiment_run_envs.py
@@ -0,0 +1,182 @@
+
+
+
+
+import os
+from os.path import expanduser
+home = expanduser("~")
+
+import json
+import subprocess
+
+import model_specs as ms
+
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print ('Made dir', path) 
+
+
+def print_stuff():
+    print ()
+    print ('Exp Name:', exp_name)
+    print (envs)
+    print ([m['name'] for m in models_list])
+    print ('Noframskip', noFrameSkip)
+    print ('Iters', iters)
+    print ('Num_frames', num_frames)
+    print ('save_interval', save_interval)
+    print ('which_gpu', which_gpu)
+    print ('num_processes', num_processes)
+    print ()
+    print ()
+
+
+
+
+
+
+# Experiment 
+##################
+exp_name = 'confirm_a2c_breakout1'
+envs = ['Breakout']  #['Pong']  #  #['Enduro'] #['Kangaroo'] ## #['Freeway'] ###,'Seaquest',,,, 'BeamRider', 'Alien', 
+            # 'Amidar','Assault', 'Freeway',
+            # 'MontezumaRevenge','Venture','Zaxxon','PrivateEye', 'Gopher']
+models_list =  [ms.a2c_adam]  #[mf.a2c_dropout]  mf.ppo_linear] # [mf.ppo_v1]# [mf.a2c_long] 
+which_gpu = 0
+noFrameSkip = True
+iters = 1
+num_frames = 6e6
+save_interval=5e5 #save model params and videos
+num_processes=20
+seed_offset = 0
+#####################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+code_location = home+"/Other_Code/RL4/rl_nov2017_2/"
+exp_path = home + '/Documents/tmp/' + exp_name+'/'
+
+make_dir(exp_path)
+
+if noFrameSkip:
+    envs = [x+'NoFrameskip-v4' for x in envs]
+print_stuff()
+
+for env in envs:
+
+    env_path = exp_path +env
+    make_dir(env_path)
+
+    for model_dict in models_list:
+
+        model_path = env_path +'/'+ model_dict['name']
+        make_dir(model_path)
+
+        for iter_i in range(seed_offset,seed_offset+iters):
+
+            iter_path = model_path +'/'+ 'seed'+str(iter_i)
+            make_dir(iter_path)
+
+            model_dict['exp_name'] = exp_name
+            model_dict['exp_path'] = exp_path
+            model_dict['seed']=iter_i
+            model_dict['save_to']=iter_path
+            model_dict['num_frames']=num_frames
+            model_dict['env'] = env
+            model_dict['cuda'] = True
+            model_dict['which_gpu'] = which_gpu
+            model_dict['save_interval'] = save_interval
+            model_dict['num_processes'] = num_processes
+
+            print (env, model_dict['name'], iter_i)
+
+            #write to json
+            json_path = iter_path+'/model_dict.json'
+            with open(json_path, 'w') as outfile:
+                json.dump(model_dict, outfile,sort_keys=True, indent=4)
+
+            #train model
+            subprocess.call("(cd "+code_location+" && python train3.py --m {})".format(json_path), shell=True) 
+            print('')
+
+    print_stuff()
+
+print ('Done.')
+
+
+
+
+
+
+#ENVIRONMENT LIST
+
+# Alien
+# Amidar
+# Assault
+# Asterix
+# Asteroids
+# Atlantis
+# BankHeist
+# BattleZone
+# BeamRider
+# Bowling
+# Boxing
+# Breakout
+# Centipede
+# ChopperCommand
+# CrazyClimber
+# DemonAttack
+# DoubleDunk
+# Enduro
+# FishingDerby
+# Freeway
+# Frostbite
+# Gopher
+# Gravitar
+# IceHockey
+# Jamesbond
+# Kangaroo
+# Skipping
+# Krull
+# KungFuMaster
+# MontezumaRevenge
+# MsPacman
+# NameThisGame
+# Pitfall
+# Pong
+# PrivateEye
+# Qbert
+# Riverraid
+# RoadRunner
+# Robotank
+# Seaquest
+# SpaceInvaders
+# StarGunner
+# Tennis
+# TimePilot
+# Tutankham
+# UpNDown
+# Venture
+# VideoPinball
+# WizardOfWor
+# Zaxxon
+
+
+
+
+
+
