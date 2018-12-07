21f1f32892472a63e0f9041dc71023b226e77648
chriscremer <chris.a.cremer@gmail.com>
2018-12-07 13:03:56 -0500

    u

--- VAE2/run_boltz.sh
+++ VAE2/run_boltz.sh
@@ -2,11 +2,14 @@
 
 
 
-/home/ccremer/anaconda3/bin/python run_vae_cifar.py --exp_name "test" \
-								--z_size 200 \
+/home/ccremer/anaconda3/bin/python run_vae_cifar.py --exp_name "vae_z500" \
+								--z_size 500 --batch_size 64 \
 								--which_gpu '0' \
 								--data_dir "$HOME/Documents/" \
 								--save_to_dir "$HOME/Documents/VAE2_exps/" \
+								--display_step 500 --start_storing_data_step 2001 \
+								--viz_steps 5000  --trainingplot_steps 5000 \
+								--save_params_step 50000 --max_steps 400000 
 								# --params_load_dir "$HOME/Documents/train_withmultipleinfnets/params/" \
 								# --model_load_step 250000 
 
